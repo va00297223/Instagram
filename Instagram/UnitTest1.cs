b7254ec461ffdfaee751b166a357c005ffed5ccd
amrit692000 <156972890+amrit692000@users.noreply.github.com>
2024-04-19 16:56:14 -0400

    Update UnitTest1.cs

--- Instagram/UnitTest1.cs
+++ Instagram/UnitTest1.cs
@@ -75,6 +75,7 @@ namespace ConsoleApp1
             }
         }
 
+        // Test-2: Entering an invalid email address
         static void EnterInvalidEmailAddress()
         {
             var driver = new EdgeDriver();
@@ -131,7 +132,65 @@ namespace ConsoleApp1
                 driver.Quit();
             }
         }
+        // Test-3: Entering an invalid password
+        static void EnterInvalidPassword()
+        {
+            var driver = new EdgeDriver();
+            try
+            {
+                driver.Url = "https://www.instagram.com/accounts/emailsignup/";
 
+                var emailOrPhoneField = driver.FindElement(By.Name("emailOrPhone"));
+                emailOrPhoneField.SendKeys("xyz@gmail.com");
+
+                var fullNameField = driver.FindElement(By.Name("fullName"));
+                fullNameField.SendKeys("XYZ ABC");
+
+                var usernameField = driver.FindElement(By.Name("username"));
+                usernameField.SendKeys("XYZ123");
+
+                var passwordField = driver.FindElement(By.Name("password"));
+                passwordField.SendKeys("mmmmmmmm"); // Enter an invalid password
+
+                var signupButton = driver.FindElement(By.CssSelector("button[type='submit']"));
+
+                // Check if the signup button is enabled before clicking
+                if (signupButton.Enabled)
+                {
+                    // Click the button
+                    signupButton.Click();
+                }
+                else
+                {
+                    Console.WriteLine("Signup button is disabled.");
+                    // Handle the disabled button scenario
+                }
+
+                Thread.Sleep(5000); // This is just for demonstration, replace with explicit wait for any expected condition
+
+                var errorMessages = driver.FindElements(By.CssSelector(".sds-control-status.error")).Select((x) => x.Text);
+
+                if (errorMessages.Any())
+                {
+                    foreach (var errorMessage in errorMessages)
+                    {
+                        Console.WriteLine(errorMessage);
+                    }
+                    Console.WriteLine("Test-3 is Passed.");
+                }
+                else
+                {
+                    Console.WriteLine("Test-3 is failed.");
+                }
+                Console.ReadLine();
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
 
         
     
